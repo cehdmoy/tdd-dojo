2e17ec20cf115e0a650575cdecb4f7a8ced34748
Roberto Moya Suárez <roberto.moya.suarez@gmail.com>
2019-03-15 09:22:20 -0300

    Firt code commit

---
+++ src/main/java/cl/scop/backend/tdddojo/TddDojoApplication.java
@@ -0,0 +1,13 @@
+package cl.scop.backend.tdddojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/scop/backend/tdddojo/TddDojoApplicationTests.java
@@ -0,0 +1,16 @@
+package cl.scop.backend.tdddojo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TddDojoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
