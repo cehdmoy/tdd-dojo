2b01178946638ff2a243de12ee814cde258ad1b3
Roberto Moya Suárez <roberto.moya.suarez@gmail.com>
2019-03-15 14:57:45 -0300

    Forgotten Servivce component

---
+++ src/main/java/cl/scop/backend/tdddojo/service/AccountService.java
@@ -0,0 +1,18 @@
+package cl.scop.backend.tdddojo.service;
+
+import cl.scop.backend.tdddojo.model.bff.AccountBffResume;
+import cl.scop.backend.tdddojo.model.core.AccountCoreResume;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountService {
+
+
+    private List<AccountBffResume> buildAccountBFF(List<AccountCoreResume> accountCoreResumes) {
+        return null;
+    }
+
+}
